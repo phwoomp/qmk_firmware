64e45692f678141568711fb711e7a4ab1520f2a5
Merijn van Moll <phwoomp@merijnvanmoll.nl>
2025-03-08 10:49:41 +0100

    add VOLUP VOLDOWN SLEEP

--- keyboards/kaba/keymaps/default/keymap.c
+++ keyboards/kaba/keymaps/default/keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_L2] = LAYOUT(
 //      /           /           /           /           /           /           /           /                       /           /           /           /           /           /           /           /
-        QK_RBT,     _______,    KC_HOME,    KC_UP  ,    KC_END ,    KC_PGUP,                                                                KC_PSLS,    KC_P7,      KC_P8,      KC_P9,      KC_PMNS,    KC_PPLS,
+        KC_SLEP,     _______,    KC_HOME,    KC_UP  ,    KC_END ,   KC_PGUP,                                                                KC_PSLS,    KC_P7,      KC_P8,      KC_P9,      KC_PMNS,    KC_PPLS,
                     _______,    KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_PGDN,    _______,    _______,                KC_PENT,    MO(_Fn),    KC_PAST,    KC_P4,      KC_P5,      KC_P6,      KC_PDOT,
                     _______,    _______,    _______,    _______,    _______,    _______,                                        _______,    KC_PDOT,    KC_P1,      KC_P2,      KC_P3,      KC_P0
     ),
@@ -77,8 +77,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_Fn] = LAYOUT(
 //      /           /           /           /           /           /           /           /                       /           /           /           /           /           /           /           /
         QK_RBT,     KC_CAPS,    KC_NUM ,    _______,    _______,    TG(_NOITA),                                                             KC_PSLS,    KC_F7,      KC_F8,      KC_F9,      KC_F12,     KC_PSCR,
-                    _______,    _______,    _______,    _______,    _______,    _______,    KC_PSCR,                _______,    _______,    KC_PAST,    KC_F4,      KC_F5,      KC_F6,      KC_F11,
-                    _______,    _______,    _______,    _______,    _______,    _______,                                        _______,    KC_PDOT,    KC_F1,      KC_F2,      KC_F3,      KC_F10
+                    _______,    _______,    _______,    _______,    _______,    KC_VOLU,    KC_PSCR,                _______,    _______,    KC_PAST,    KC_F4,      KC_F5,      KC_F6,      KC_F11,
+                    _______,    _______,    _______,    _______,    _______,    KC_VOLD,                                        _______,    KC_PDOT,    KC_F1,      KC_F2,      KC_F3,      KC_F10
     ),
 
     [_NOITA] = LAYOUT(
